b47da62b393a47867c23687bcaf003ecb4f0b5e4
azad-technext <azadashraf@technext.it>
2025-10-31 14:51:24 +0600

    chore(client): intigrate aurora

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,7 +1,21 @@
-import { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9001',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prium.github.io',
+        pathname: '/aurora/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
